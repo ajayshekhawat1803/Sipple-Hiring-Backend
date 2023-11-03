a6748947f3a400ae1f6c4f62c6d55b19575f4a2b
ajayshekhawat1803 <ajayshekhawat1803@gmail.com>
2023-11-03 19:02:43 +0530

    Added .env and gitignore

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+dotenv.config();
 
-const password = encodeURIComponent("database");
-const uri = `mongodb+srv://ajayshekhawat1803:${password}@cluster0.l305iue.mongodb.net/SimpleHiring?retryWrites=true&w=majority`;
-const connection = mongoose.connect(uri);
+const connection = mongoose.connect(process.env.MONGODB_URI);
 
 export default connection
