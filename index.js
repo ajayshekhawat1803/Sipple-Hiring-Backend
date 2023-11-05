7bfaf14d32036c461e101bfb47705a3d1451db54
ajayshekhawat1803 <ajayshekhawat1803@gmail.com>
2023-11-05 19:49:13 +0530

    Making ready for deployment

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ import RecruiterRouter from './Routes/RecruiterRouter.js'
 import AdminRouter from './Routes/AdminRouter.js'
 import Homerouter from './Routes/HomeRouter.js'
 const app = express()
-const Port = process.env.PORT ||4000
+const Port = process.env.PORT || 4000
 app.use(express.json());
 
 app.use(cors())
 
-app.use("/uploads",express.static('uploads'))
+app.use("/uploads", express.static('uploads'))
 
 app.use("/recruiter", RecruiterRouter)
 app.use("/admin", AdminRouter)
-app.use("/home",Homerouter)
+app.use("/home", Homerouter)
+app.get("/", (req, res) => {
+    res.json({ message: "Server is working and responding correctly" })
+})
 
 connection.then(() => {
     app.listen(Port, () => {
